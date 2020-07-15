18c8fbf2e2f6ec9329bc73087ffdb8b478602b7a
hollandcodeandgraphics <hollandcodeandgraphics@gmail.com>
2020-07-15 08:05:42 -0700

    updated vega_id

--- multisigcontrol/MultisigControl_ABI.js
+++ multisigcontrol/MultisigControl_ABI.js
@@ -1,9 +1,177 @@
 multisigControl_abi =  [
     {
-      "inputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "constructor"
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "nonce",
+          "type": "uint256"
+        }
+      ],
+      "name": "Asset_Blacklisted",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "new_minimum",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "nonce",
+          "type": "uint256"
+        }
+      ],
+      "name": "Asset_Deposit_Minimum_Set",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "user_address",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "bytes32",
+          "name": "vega_public_key",
+          "type": "bytes32"
+        }
+      ],
+      "name": "Asset_Deposited",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
+        {
+          "indexed": true,
+          "internalType": "bytes32",
+          "name": "vega_id",
+          "type": "bytes32"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "nonce",
+          "type": "uint256"
+        }
+      ],
+      "name": "Asset_Whitelisted",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "user_address",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "nonce",
+          "type": "uint256"
+        }
+      ],
+      "name": "Asset_Withdrawn",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "multisig_control_source",
+          "type": "address"
+        }
+      ],
+      "name": "Multisig_Control_Set",
+      "type": "event"
     },
     {
       "anonymous": false,
@@ -25,102 +193,6 @@ multisigControl_abi =  [
       "type": "event"
     },
     {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "address",
-          "name": "new_signer",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "nonce",
-          "type": "uint256"
-        }
-      ],
-      "name": "SignerAdded",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "address",
-          "name": "new_signer",
-          "type": "address"
-        }
-      ],
-      "name": "SignerAdded_Admin",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "address",
-          "name": "old_signer",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "nonce",
-          "type": "uint256"
-        }
-      ],
-      "name": "SignerRemoved",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "address",
-          "name": "old_signer",
-          "type": "address"
-        }
-      ],
-      "name": "SignerRemoved_Admin",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "uint16",
-          "name": "new_threshold",
-          "type": "uint16"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "nonce",
-          "type": "uint256"
-        }
-      ],
-      "name": "ThresholdSet",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "uint16",
-          "name": "new_threshold",
-          "type": "uint16"
-        }
-      ],
-      "name": "ThresholdSet_Admin",
-      "type": "event"
-    },
-    {
       "constant": true,
       "inputs": [],
       "name": "isOwner",
@@ -133,6 +205,15 @@ multisigControl_abi =  [
       ],
       "payable": false,
       "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "kill",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
       "type": "function"
     },
     {
@@ -175,24 +256,48 @@ multisigControl_abi =  [
       "type": "function"
     },
     {
-      "constant": true,
+      "constant": false,
       "inputs": [
         {
           "internalType": "address",
-          "name": "target",
+          "name": "asset_source",
           "type": "address"
-        }
-      ],
-      "name": "get_msg_bytes",
-      "outputs": [
+        },
         {
-          "internalType": "bytes",
-          "name": "",
-          "type": "bytes"
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bytes32",
+          "name": "vega_id",
+          "type": "bytes32"
         }
       ],
+      "name": "whitelist_asset_admin",
+      "outputs": [],
       "payable": false,
-      "stateMutability": "view",
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        }
+      ],
+      "name": "blacklist_asset_admin",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
       "type": "function"
     },
     {
@@ -200,11 +305,16 @@ multisigControl_abi =  [
       "inputs": [
         {
           "internalType": "address",
-          "name": "target",
+          "name": "asset_source",
           "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
         }
       ],
-      "name": "get_msg_hash",
+      "name": "get_vega_id",
       "outputs": [
         {
           "internalType": "bytes32",
@@ -217,57 +327,74 @@ multisigControl_abi =  [
       "type": "function"
     },
     {
+      "constant": true,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "vega_id",
+          "type": "uint256"
+        }
+      ],
+      "name": "get_asset_source_and_asset_id",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "internalType": "bytes32",
+          "name": "vega_id",
+          "type": "bytes32"
+        }
+      ],
+      "name": "get_asset_source_and_asset_id",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
       "constant": false,
       "inputs": [
         {
           "internalType": "address",
-          "name": "new_signer",
+          "name": "asset_source",
           "type": "address"
-        }
-      ],
-      "name": "add_signer_admin",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [
+        },
         {
-          "internalType": "address",
-          "name": "old_signer",
-          "type": "address"
-        }
-      ],
-      "name": "remove_signer_admin",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
         {
-          "internalType": "uint16",
-          "name": "new_threshold",
-          "type": "uint16"
-        }
-      ],
-      "name": "set_threshold_admin",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "internalType": "uint16",
-          "name": "new_threshold",
-          "type": "uint16"
+          "internalType": "bytes32",
+          "name": "vega_id",
+          "type": "bytes32"
         },
         {
           "internalType": "uint256",
@@ -280,7 +407,7 @@ multisigControl_abi =  [
           "type": "bytes"
         }
       ],
-      "name": "set_threshold",
+      "name": "whitelist_asset",
       "outputs": [],
       "payable": false,
       "stateMutability": "nonpayable",
@@ -291,8 +418,13 @@ multisigControl_abi =  [
       "inputs": [
         {
           "internalType": "address",
-          "name": "new_signer",
+          "name": "asset_source",
           "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
         },
         {
           "internalType": "uint256",
@@ -305,7 +437,7 @@ multisigControl_abi =  [
           "type": "bytes"
         }
       ],
-      "name": "add_signer",
+      "name": "blacklist_asset",
       "outputs": [],
       "payable": false,
       "stateMutability": "nonpayable",
@@ -316,12 +448,22 @@ multisigControl_abi =  [
       "inputs": [
         {
           "internalType": "address",
-          "name": "old_signer",
+          "name": "asset_source",
           "type": "address"
         },
         {
           "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
           "name": "nonce",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "minimum_amount",
           "type": "uint256"
         },
         {
@@ -330,7 +472,7 @@ multisigControl_abi =  [
           "type": "bytes"
         }
       ],
-      "name": "remove_signer",
+      "name": "set_deposit_minimum",
       "outputs": [],
       "payable": false,
       "stateMutability": "nonpayable",
@@ -340,22 +482,102 @@ multisigControl_abi =  [
       "constant": false,
       "inputs": [
         {
-          "internalType": "bytes",
-          "name": "signatures",
-          "type": "bytes"
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
         },
         {
-          "internalType": "bytes",
-          "name": "message",
-          "type": "bytes"
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "expiry",
+          "type": "uint256"
         },
         {
           "internalType": "uint256",
           "name": "nonce",
           "type": "uint256"
+        },
+        {
+          "internalType": "bytes",
+          "name": "signatures",
+          "type": "bytes"
         }
       ],
-      "name": "verify_signatures",
+      "name": "withdraw_asset",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bytes32",
+          "name": "vega_public_key",
+          "type": "bytes32"
+        }
+      ],
+      "name": "deposit_asset",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "new_multisig_contract_address",
+          "type": "address"
+        }
+      ],
+      "name": "set_multisig_control",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        }
+      ],
+      "name": "is_asset_whitelisted",
       "outputs": [
         {
           "internalType": "bool",
@@ -364,18 +586,29 @@ multisigControl_abi =  [
         }
       ],
       "payable": false,
-      "stateMutability": "nonpayable",
+      "stateMutability": "view",
       "type": "function"
     },
     {
       "constant": true,
-      "inputs": [],
-      "name": "get_valid_signer_count",
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "asset_source",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "asset_id",
+          "type": "uint256"
+        }
+      ],
+      "name": "get_deposit_minimum",
       "outputs": [
         {
-          "internalType": "uint8",
+          "internalType": "uint256",
           "name": "",
-          "type": "uint8"
+          "type": "uint256"
         }
       ],
       "payable": false,
@@ -385,58 +618,16 @@ multisigControl_abi =  [
     {
       "constant": true,
       "inputs": [],
-      "name": "get_current_threshold",
+      "name": "get_multisig_control_address",
       "outputs": [
-        {
-          "internalType": "uint16",
-          "name": "",
-          "type": "uint16"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [
         {
           "internalType": "address",
-          "name": "signer_address",
+          "name": "",
           "type": "address"
-        }
-      ],
-      "name": "is_valid_signer",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "nonce",
-          "type": "uint256"
-        }
-      ],
-      "name": "is_nonce_used",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
         }
       ],
       "payable": false,
       "stateMutability": "view",
       "type": "function"
     }
-  ];
+  ]
